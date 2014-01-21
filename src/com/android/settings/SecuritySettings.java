2c767271fef23d996b9c08690a6d1c7be6bae354
jyates <jared.yates@gmail.com>
2014-01-22 00:04:57 +1300

    [2/2] Settings: Make menu unlock configurable
    For devices with hardware menu keys, menu unlock is turned on by default
    This can cause unintentional unlocks. This commit adds the ability to turn
    Menu Unlock on or off as desired (Settings -> Security)
    
    System Default is for menu unlock to be ON. This default value can be
    overriden using a device overlay:
    
    overlay/frameworks/base/packages/Keyguard/res/values/config.xml
    <!-- Allow the menu hard key to be disabled in LockScreen on some devices -->
        <bool name="config_disableMenuKeyInLockScreen">true</bool>
    
    This commit requires the matching commit from frameworks_bases repo:
    https://gerrit.slimroms.net/#/c/2394/
    
    Based on: https://gerrit.omnirom.org/#/c/3318/
    
    PS7: update overlay location
    PS8: fix config value retrieval
    PS9: rebase
    PS10: catch NPE of mMenuUnlock if screen lock 'pattern' is used
    
    Change-Id: I8fc38b17315a050b10bb6d5fb13f6abf0f306e08
    
    Conflicts:
    
    	res/values/hyperion_strings.xml
    	src/com/android/settings/SecuritySettings.java

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -29,6 +29,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.pm.UserInfo;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
@@ -76,10 +77,15 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private static final String LOCK_NUMPAD_RANDOM = "lock_numpad_random";
     private static final String LOCK_BEFORE_UNLOCK = "lock_before_unlock";
     private static final String KEY_ADVANCED_REBOOT = "advanced_reboot";
+    private static final String MENU_UNLOCK_PREF = "menu_unlock";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_LIVELINESS_OFF = 125;
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    private static final int KEY_MASK_MENU = 0x04;
 
     // Misc Settings
     private static final String KEY_SIM_LOCK = "sim_lock";
@@ -123,8 +129,13 @@ public class SecuritySettings extends RestrictedSettingsFragment
     private CheckBoxPreference mQuickUnlockScreen;
     private ListPreference mLockNumpadRandom;
     private CheckBoxPreference mLockBeforeUnlock;
+    private CheckBoxPreference mMenuUnlock;
 
     private Preference mNotificationAccess;
+
+    // needed for menu unlock
+    private Resources keyguardResource;
+    private boolean mMenuUnlockDefault;
 
     private boolean mIsPrimary;
 
@@ -143,6 +154,16 @@ public class SecuritySettings extends RestrictedSettingsFragment
 
         mPM = getActivity().getPackageManager();
         mDPM = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+        Resources keyguardResources = null;
+        try {
+            keyguardResources = mPM.getResourcesForApplication("com.android.keyguard");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mMenuUnlockDefault = keyguardResources != null
+            ? keyguardResources.getBoolean(keyguardResources.getIdentifier(
+            "com.android.keyguard:bool/config_disableMenuKeyInLockScreen", null, null)) : false;
 
         mChooseLockSettingsHelper = new ChooseLockSettingsHelper(getActivity());
     }
@@ -329,6 +350,24 @@ public class SecuritySettings extends RestrictedSettingsFragment
                             R.string.security_enable_widgets_disabled_summary);
                 }
                 mEnableKeyguardWidgets.setEnabled(!disabled);
+            }
+        }
+
+        // Menu Unlock
+        mMenuUnlock = (CheckBoxPreference) root.findPreference(MENU_UNLOCK_PREF);
+        if (mMenuUnlock != null) {
+            int deviceKeys = getResources().getInteger(
+                    com.android.internal.R.integer.config_deviceHardwareKeys);
+            boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+            if (hasMenuKey) {
+                boolean settingsEnabled = Settings.System.getIntForUser(
+                        getContentResolver(),
+                        Settings.System.MENU_UNLOCK_SCREEN, mMenuUnlockDefault ? 0 : 1,
+                        UserHandle.USER_CURRENT) == 1;
+                mMenuUnlock.setChecked(settingsEnabled);
+                mMenuUnlock.setOnPreferenceChangeListener(this);
+            } else {
+                securityCategory.removePreference(mMenuUnlock);
             }
         }
 
@@ -719,6 +758,10 @@ public class SecuritySettings extends RestrictedSettingsFragment
                     Integer.valueOf((String) value));
             mAdvancedReboot.setValue(String.valueOf(value));
             mAdvancedReboot.setSummary(mAdvancedReboot.getEntry());
+        } else if (preference == mMenuUnlock) {
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.MENU_UNLOCK_SCREEN,
+                    ((Boolean) value) ? 1 : 0, UserHandle.USER_CURRENT);
         }
         return true;
     }
